cb95709d69008eae78e86cbcbb01337e62624c7f
estebanhirzfeld <estebanhirzfeld@gmail.com>
2022-02-15 18:29:22 -0300

    Deploy

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+$('.buttonNav--home').click(function () {
+    $('html, body').animate({ scrollTop: $('#home').offset().top }, 500);
+})
+
+$('.buttonNav--about').click(function () {
+    $('html, body').animate({ scrollTop: $('#about').offset().top }, 500);
+})
+
+$('.buttonNav--servicios').click(function () {
+    $('html, body').animate({ scrollTop: $('#servicios').offset().top }, 500);
+})
+
+$('.buttonNav--exterior').click(function () {
+    $('html, body').animate({ scrollTop: $('#exterior').offset().top }, 500);
+})
+
+$('.buttonNav--contacto').click(function () {
+    $('html, body').animate({ scrollTop: $('#contacto').offset().top }, 500);
+})
